af0c6ddacefc9ef62dabe23cd48ab96185fea710
[bhopendra] <[bhopendrasahu2899@gmail.com]>
2022-10-15 13:13:38 +0530

    Encapsulation code working

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+
+      RWOnly obj=new RWOnly();
+
+      //obj.name="bhopendra";
+      //java: name has private access in com.driver.RWOnly
+
+      obj.setName("bhopendra");
+      System.out.println(obj.getName());
+  }
 }
